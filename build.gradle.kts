224edbe3cd08a17e8fc37f9b63507e97bc65ea94
jGalvann <joaogalvan02@gmail.com>
2026-05-23 15:53:45 -0300

    Criação do projeto, e estruturacao das pastas

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+group = "com"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.serialization.kotlinx.json)
+    implementation(ktorLibs.server.auth)
+    implementation(ktorLibs.server.auth.jwt)
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.contentNegotiation)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(ktorLibs.server.websockets)
+    implementation(libs.logback.classic)
+
+    // implementação do EXPOSED
+    implementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
+
+    // implementação do HikariCP ( pool de conexões )
+    implementation("com.zaxxer:HikariCP:6.2.1")
+
+    // implementação do Driver PostgreSQL
+    implementation("org.postgresql:postgresql:42.7.4")
+
+    // Dotenv ( para ler .env )
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
---
+++ src/main/kotlin/com/plugins/DatabaseFactory.kt
@@ -0,0 +1,33 @@
+package com.plugins
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.github.cdimascio.dotenv.dotenv
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+
+    fun init() {
+        val dotenv = dotenv()
+
+        val config = HikariConfig().apply {
+            jdbcUrl         = dotenv["DATABASE_URL"]
+            username        = dotenv["DATABASE_USER"]
+            password        = dotenv["DATABASE_PASSWORD"]
+            driverClassName = "org.postgresql.Driver"
+            maximumPoolSize = 5
+            isAutoCommit    = false
+            transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        }
+
+        val dataSource = HikariDataSource(config)
+        Database.connect(dataSource)
+
+        // Cria as tabelas se não existirem
+        transaction {
+
+        }
+    }
+}
